c966cae2989d4c95d98a8910a77d0d7766506bdf
Salim Hatibu <167990287+salimhatibu@users.noreply.github.com>
2025-04-26 21:32:45 +0300

    Add files via upload

---
+++ eo.py
@@ -0,0 +1,11 @@
+def even_or_odd():
+    # Function to check if a number is even or odd
+    number = int(input("Enter a number: "))
+    # Taking input from the user and converting it to an integer
+    if number % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+even_or_odd()
+# Calling the function to execute it
